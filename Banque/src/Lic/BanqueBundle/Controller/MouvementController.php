f38ad38237d03de6e628648fc26ed85819dd7a9f
amine <amine.benhammou@etu.univ-poitiers.fr>
2018-05-16 16:28:27 +0200

    ajout entite Mouvement

--- Banque/src/Lic/BanqueBundle/Controller/MouvementController.php
+++ Banque/src/Lic/BanqueBundle/Controller/MouvementController.php
@@ -6,20 +6,120 @@ namespace Lic\BanqueBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Request;
+use Lic\BanqueBundle\Entity\Film;
+use Lic\BanqueBundle\Entity\Mouvement;
+use Lic\BanqueBundle\Entity\Critique;
 
 class MouvementController extends Controller
 {
 
 
-    public function viewAction($id, Request $request)
+
+
+
+    public function indexAction($page)
     {
 
-        $tag = $request->query->get('tag');
+        $em = $this->getDoctrine()->getEntityManager();
 
-        return $this->render('@LicBanque/Mouvement/view.html.twig', array(
-            'id'  => $id,
-            'tag' => $tag,
+        $mouvementRepository = $em->getRepository('LicBanqueBundle:Mouvement');
+
+        $listmouv = $mouvementRepository->findAll();
+
+        return $this->render('@LicBanque/Mouvement/index.html.twig', array(
+            'listmouv' => $listmouv, 'page'=>$page
         ));
     }
+
+
+
+    public function viewAction($id)
+    {
+
+        $em = $this->getDoctrine()->getEntityManager();
+        $MouvementRepository = $em->getRepository('LicBanqueBundle:Mouvement');
+
+        /** @var Mouvement $mouv */
+        $Mouv = $MouvementRepository->find($id);
+        if ($Mouv === null)
+            throw new NotFoundHttpException('Le mouvement ' . $id . ' n\'existe pas;');
+
+        $args = array(
+            'mouv' => $Mouv
+        );
+
+        return $this->render('@LicBanque/Mouvement/view.html.twig', $args);
+
+    }
+
+
+    public function addendurAction()
+
+    {
+
+        $em = $this->getDoctrine()->getEntityManager();
+
+        $Mouv = new Mouvement();
+
+        //$Mouv->setDateMouvement(2018-05-15);
+
+        $Mouv->setMontant(5000);
+
+        $Mouv->setCompte(298438);
+
+        $Mouv->setDescription("test");
+
+        $Mouv->setRepetitif(1);
+
+        $Mouv->setmembre(1);
+
+        $Mouv->setValider(true);
+
+        $Mouv->setRapprochement(true);
+
+        $em->persist($Mouv);
+
+        $em->flush();
+
+
+    return $this->redirectToRoute('lic_banque_mouvement', array('id' => $Mouv->getId() ));
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    public function menuAction($limit)
+    {
+
+        $listmouv = array(
+            array('id' => 2, 'description' => 'Salaire mai'),
+            array('id' => 5, 'description' => 'sfr avril'),
+            array('id' => 9, 'description' => 'Salaire')
+        );
+
+        return $this->render('@LicBanque/Mouvement/menu.html.twig', array(
+
+            'listmouv' => $listmouv
+        ));
+    }
+
+
 
 }
---
+++ Banque/src/Lic/BanqueBundle/Entity/Mouvement.php
@@ -0,0 +1,314 @@
+<?php
+
+namespace Lic\BanqueBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Mouvement
+ *
+ * @ORM\Table(name="mouvement")
+ * @ORM\Entity(repositoryClass="Lic\BanqueBundle\Repository\MouvementRepository")
+ */
+class Mouvement
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var float
+     *
+     * @ORM\Column(name="montant", type="float")
+     */
+    private $montant;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="date_mouvement",nullable=true , type="datetime")
+     */
+    private $dateMouvement;
+
+    /**
+     * @var bool
+     *
+     * @ORM\Column(name="valider", type="boolean")
+     */
+    private $valider;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="descriptif", type="text" , options={"default"=0})
+     */
+    private $description;
+
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id_compte", nullable=true , type="integer")
+     */
+    private $compte;
+
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id_membre", nullable=true , type="integer")
+     */
+    private $membre;
+
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id_moyen_paiement",nullable=true , type="integer")
+     */
+    private $moyenPaiement;
+
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id_repetitif", nullable=true , type="integer")
+     */
+    private $repetitif;
+
+    /**
+     * @var bool
+     *
+     * @ORM\Column(name="rapprochement", type="boolean" ,options={"default"=0})
+     */
+    private $rapprochement;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set montant
+     *
+     * @param float $montant
+     *
+     * @return Mouvement
+     */
+    public function setMontant($montant)
+    {
+        $this->montant = $montant;
+
+        return $this;
+    }
+
+    /**
+     * Get montant
+     *
+     * @return float
+     */
+    public function getMontant()
+    {
+        return $this->montant;
+    }
+
+    /**
+     * Set dateMouvement
+     *
+     * @param \DateTime $dateMouvement
+     *
+     * @return Mouvement
+     */
+    public function setDateMouvement($dateMouvement)
+    {
+        $this->dateMouvement = $dateMouvement;
+
+        return $this;
+    }
+
+    /**
+     * Get dateMouvement
+     *
+     * @return \DateTime
+     */
+    public function getDateMouvement()
+    {
+        return $this->dateMouvement;
+    }
+
+    /**
+     * Set valider
+     *
+     * @param boolean $valider
+     *
+     * @return Mouvement
+     */
+    public function setValider($valider)
+    {
+        $this->valider = $valider;
+
+        return $this;
+    }
+
+    /**
+     * Get valider
+     *
+     * @return bool
+     */
+    public function getValider()
+    {
+        return $this->valider;
+    }
+
+    /**
+     * Set description
+     *
+     * @param string $description
+     *
+     * @return Mouvement
+     */
+    public function setDescription($description)
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    /**
+     * Get description
+     *
+     * @return string
+     */
+    public function getDescription()
+    {
+        return $this->description;
+    }
+
+    /**
+     * Set compte
+     *
+     * @param integer $compte
+     *
+     * @return Mouvement
+     */
+    public function setCompte($compte)
+    {
+        $this->compte = $compte;
+
+        return $this;
+    }
+
+    /**
+     * Get compte
+     *
+     * @return int
+     */
+    public function getCompte()
+    {
+        return $this->compte;
+    }
+
+    /**
+     * Set membre
+     *
+     * @param integer $membre
+     *
+     * @return Mouvement
+     */
+    public function setmembre($membre)
+    {
+        $this->membre = $membre;
+
+        return $this;
+    }
+
+    /**
+     * Get membre
+     *
+     * @return int
+     */
+    public function getmembre()
+    {
+        return $this->membre;
+    }
+
+    /**
+     * Set moyenPaiement
+     *
+     * @param integer $moyenPaiement
+     *
+     * @return Mouvement
+     */
+    public function setMoyenPaiement($moyenPaiement)
+    {
+        $this->moyenPaiement = $moyenPaiement;
+
+        return $this;
+    }
+
+    /**
+     * Get moyenPaiement
+     *
+     * @return int
+     */
+    public function getMoyenPaiement()
+    {
+        return $this->moyenPaiement;
+    }
+
+    /**
+     * Set repetitif
+     *
+     * @param integer $repetitif
+     *
+     * @return Mouvement
+     */
+    public function setRepetitif($repetitif)
+    {
+        $this->repetitif = $repetitif;
+
+        return $this;
+    }
+
+    /**
+     * Get repetitif
+     *
+     * @return int
+     */
+    public function getRepetitif()
+    {
+        return $this->repetitif;
+    }
+
+    /**
+     * Set rapprochement
+     *
+     * @param boolean $rapprochement
+     *
+     * @return Mouvement
+     */
+    public function setRapprochement($rapprochement)
+    {
+        $this->rapprochement = $rapprochement;
+
+        return $this;
+    }
+
+    /**
+     * Get rapprochement
+     *
+     * @return bool
+     */
+    public function getRapprochement()
+    {
+        return $this->rapprochement;
+    }
+}
+
